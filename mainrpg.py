47c9f4eea4f8f79812bcffc2b080242f0e225424
DeolaRichard <adekunlerichie5@gmail.com>
2023-07-28 19:23:21 +0100

    writing logic to get randomised characters

--- mainrpg.py
+++ mainrpg.py
@@ -1,5 +1,15 @@
 # ask if user wants to get random password
-# if yes, ask how long the password should be
+# if yes, ask how long the password should
 # generate the password
 # print the password
+
+import string
+import random
+
+characters = list(string.ascii_letters + string.digits + "!@#$%&()^")
+
+def generate_password():
+    password_length = int(input("How long do you want your password to be? "))
+
+    random.shuffle(characters)
 

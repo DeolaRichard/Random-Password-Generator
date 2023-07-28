cbd8c46194bd5300704e19933e91e1990f285285
DeolaRichard <adekunlerichie5@gmail.com>
2023-07-28 19:36:23 +0100

    shuffled gotten characters and used the .join fx to convert a list to string

--- mainrpg.py
+++ mainrpg.py
@@ -2,14 +2,26 @@
 # if yes, ask how long the password should
 # generate the password
 # print the password
+# if answer at line 2 is no, exit the program.
 
 import string
 import random
 
 characters = list(string.ascii_letters + string.digits + "!@#$%&()^")
 
+
 def generate_password():
     password_length = int(input("How long do you want your password to be? "))
 
     random.shuffle(characters)
 
+    password = []
+
+    for x in range(password_length):
+        password.append(random.choice(characters))
+
+        random.shuffle(password)
+
+        password = "".join(password)
+
+        print(password)

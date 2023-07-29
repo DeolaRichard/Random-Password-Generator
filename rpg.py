db6242c371afdf2214dc66aea8455f8a429a0386
DeolaRichard <adekunlerichie5@gmail.com>
2023-07-29 09:17:08 +0100

    learning else using for loops

--- rpg.py
+++ rpg.py
@@ -16,3 +16,13 @@ def fib2(n):  # return fibonacci series up to n
         a, b = b, a + b
         return result
 
+
+# using for else in loops
+nums = [11, 13, 19, 18, 6, 9]
+
+for num in nums:
+    if num % 5 == 0:
+        print(num)
+        break  # to jump out of the loop
+else:
+        print("Not Found")

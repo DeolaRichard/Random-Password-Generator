e3d0a5669f118e334a7be1c7671eacdac5fd18c6
DeolaRichard <adekunlerichie5@gmail.com>
2023-07-30 04:06:35 +0100

    Added a more efficient code to check for prime number

--- rpg.py
+++ rpg.py
@@ -1,3 +1,6 @@
+import math
+
+
 # fibonacci numbers module
 
 def fib(n):  # write fibonacci series up to n
@@ -28,10 +31,38 @@ else:
     print("Not Found")
 
 # To find if a number is prime or not
-num = int(input("Write a number to check: "))  # accept input from user
-for i in range(2, num):
-    if num % i == 0:  # check if the number is divisible by each number in iteration
+nums = int(input("Write a number to check: "))  # accept input from user
+for i in range(2, nums):
+    if nums % i == 0:  # check if the number is divisible by each number in iteration
         print("Not a Prime Number")
         break
 else:
     print("Is a Prime Number")
+
+
+# A more efficient way to solve this
+
+
+def is_prime(num):
+    if num < 2:
+        return False
+
+    if num == 2:
+        return True
+
+    if num % 2 == 0:
+        return False
+
+    # Only need to check up to the square root of the number
+    for i in range(3, int(math.sqrt(num)) + 1, 2):
+        if num % i == 0:
+            return False
+
+    return True
+
+
+num = int(input("Write a number to check: "))
+if is_prime(num):
+    print("Is a Prime Number")
+else:
+    print("Not a Prime Number")
